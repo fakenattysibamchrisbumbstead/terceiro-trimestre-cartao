7f63ca8fce1d7b8d22cf82a65c1a299ac786fd29
Felipe <143546223+fakenattysibamchrisbumbstead@users.noreply.github.com>
2024-11-22 20:09:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'Kickboxing',
+    'Quantas faixas existem?',
+    'Normalmente, branca, amarela, verde, laranja, azul, roxa, marrom e preta.'
+)
+
+criaCartao(
+    'Kickboxing',
+    'Quais são as modalidades padrão WAKO?',
+    'Kick ligth, light contact, point fighting e K1.'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação 2.0',
+    '0 que é JavaScript?',
+    '0 JavaScript é uma linguagem de programação'
+)
